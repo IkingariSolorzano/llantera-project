eb773fa7cc6780fde440e2c186b949dd8179fdbb
Ikingari <david.solorzano@umich.mx>
2025-11-27 13:40:02 -0600

    ruta para el back

--- trezo/src/app/core/config/api.config.ts
+++ trezo/src/app/core/config/api.config.ts
@@ -2,4 +2,4 @@ import { InjectionToken } from '@angular/core';
 
 export const API_BASE_URL = new InjectionToken<string>('API_BASE_URL');
 
-export const DEFAULT_API_BASE_URL = 'https://ikingarisolorzano.com/api';
+export const DEFAULT_API_BASE_URL = 'https://ikingarisolorzano.com/llantera/api';
