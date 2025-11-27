e48ae2b5cdae4960129fad038afd58cce329f8cf
Ikingari <david.solorzano@umich.mx>
2025-11-27 13:10:36 -0600

    cambios para staging

--- llantera-hex/internal/adapters/http/middleware/cors.go
+++ llantera-hex/internal/adapters/http/middleware/cors.go
@@ -12,7 +12,7 @@ func WithCORS(next http.Handler) http.Handler {
 	}
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "https://ikingarisolorzano.com")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
